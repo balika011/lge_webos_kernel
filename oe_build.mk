8c0de78a4775ba609042e0ca9ee4fb53d1d5b71f
p4admin <p4admin@mediatek.com>
2015-01-10 19:13:42 +0800

    [p4-to-git] ChangeNo:1890009 [Auto_Sync_LG] 1889992 [DTV00621374] [LGA5LR]OE Build issue. .
    compile in oe environment.
    .

--- oe_build.mk
+++ oe_build.mk
@@ -3,6 +3,7 @@ all :
 			make ARCH=arm mt5882_A5LR_smp_mod_defconfig
 			make ARCH=arm uImage -j64 CONFIG_CROSS_COMPILE=/mtkoss/gnuarm/arm-lg115x-linux-gnueabi-4.8-2014.08-1-x86_64/bin/arm-lg115x-linux-gnueabi-
 			chmod +x kernel.sh
+			chmod +x ./../boot/uboot/tools/mkepk
 			./kernel.sh
 clean:
 			make clean
