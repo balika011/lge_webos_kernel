1141777041a9476e5830c8d2282eecca6f11c679
p4admin <p4admin@mediatek.com>
2015-02-25 14:32:54 +0800

    [MTWTV-1611]Kernel stop when making snapshot . add solution for cpu hotplug and enable snapshot .
    ---- mtk usage begin ---
    MTK_Submitter: hongjun.chu
    MTK_MP_CL: 1932032
    MTK_RLS_CL: 1932037
    MTK_CR_ID: DTV00635257
    ---- mtk usage end ---
    .

--- arch/arm/mach-mt53xx/hotplug.c
+++ arch/arm/mach-mt53xx/hotplug.c
@@ -77,105 +77,15 @@ void inner_dcache_flush_L2(void)
 }
 static inline void cpu_enter_lowpower(unsigned int cpu)
 {
-	unsigned int v;
-    //HOTPLUG_INFO("cpu_enter_lowpower\n");
 
-    /* Cluster off Only MT5890 ES or above uses this, Gazelle and Capri need to jump to another condition */
-    if ((mt53xx_get_ic_version() > IC_VER_MT5890_AC) && ((cpu==3 && cpu_online(2)==0) || (cpu==2 && cpu_online(3)==0)))
-    {
-
-		__disable_dcache__inner_flush_dcache_L1__inner_flush_dcache_L2();
-#if 0
-        /* Clear the SCTLR C bit to prevent further data cache allocation */
-	asm volatile(
-		"mcr	p15, 0, %1, c7, c5, 0\n"    // invalidate entire instruction cache
-	"	mcr	p15, 0, %1, c7, c10, 4\n"   // dsb
-	"	dmb\n"   // dmb
-	"	mrc	p15, 0, %0, c1, c0, 0\n"
-	"	bic	%0, %0, %2\n"		    // disable D cache
-	"	mcr	p15, 0, %0, c1, c0, 0\n"
-	  : "=&r" (v)
-	  : "r" (0), "Ir" (CR_C), "Ir" (0x40)
-	  : "cc");
-        isb();
-        dsb();
-
-        /* Clean and invalidate all data from the L1, L2 data cache */
-        inner_dcache_flush_all();
-#endif
-        /* Switch the processor from SMP mode to AMP mode by clearing the ACTLR SMP bit */
-    	asm volatile(
-    	"	mrc	p15, 0, %0, c1, c0, 1\n"
-    	"	bic	%0, %0, %3\n"		    // ACTLR.SMP | ACTLR.FW
-    	"	mcr	p15, 0, %0, c1, c0, 1\n"
-    	  : "=&r" (v)
-    	  : "r" (0), "Ir" (CR_C), "Ir" (0x40)
-    	  : "cc");
-
-        isb();
-        dsb();
-
-        BIM_ClusterDisable(1);
-    }
-    else
-    {
-	   __disable_dcache__inner_flush_dcache_L1__inner_clean_dcache_L2();
-#if 0	
-    	asm volatile(
-    		"mcr	p15, 0, %1, c7, c5, 0\n"    // invalidate entire instruction cache
-    	"	mcr	p15, 0, %1, c7, c10, 4\n"   // dsb
-    	"	dmb\n"   // dmb
-    	"	mrc	p15, 0, %0, c1, c0, 0\n"
-    	"	bic	%0, %0, %2\n"		    // disable D cache
-    	"	mcr	p15, 0, %0, c1, c0, 0\n"
-    	  : "=&r" (v)
-    	  : "r" (0), "Ir" (CR_C), "Ir" (0x40)
-    	  : "cc");
-        /* Clear the SCTLR C bit to prevent further data cache allocation */
-        //__disable_dcache();
-        isb();
-        dsb();
-        /* Clean and invalidate all data from the L1 data cache */
-        inner_dcache_flush_L1();
-#endif
-    	udelay(2000);
-    	__asm__ __volatile__("clrex"); // Execute a CLREX instruction
-    	asm volatile(
-    	"	mrc	p15, 0, %0, c1, c0, 1\n"
-    	"	bic	%0, %0, %3\n"		    // ACTLR.SMP | ACTLR.FW
-    	"	mcr	p15, 0, %0, c1, c0, 1\n"
-    	  : "=&r" (v)
-    	  : "r" (0), "Ir" (CR_C), "Ir" (0x40)
-    	  : "cc");
-    	isb();
-    }
 }
 
 static inline void cpu_leave_lowpower(unsigned int cpu)
 {
-	unsigned int v;
-
-#if !defined(CC_NO_EXT_SCU_SETTING)
 	isb();
 	dmb();
 	dsb();
 	isb();
-	scu_power_mode(scu_base_addr(), SCU_PM_NORMAL);
-#endif
-	isb();
-	dmb();
-	dsb();
-	isb();
-	asm volatile(
-		"	mrc p15, 0, %0, c1, c0, 1\n"
-		"	orr %0, %0, %2\n"			// ACTLR.SMP
-		"	mcr p15, 0, %0, c1, c0, 1\n"
-		"	mrc p15, 0, %0, c1, c0, 0\n"
-		"	orr %0, %0, %1\n"			// enable D cache
-		"	mcr p15, 0, %0, c1, c0, 0\n"
-		  : "=&r" (v)
-		  : "Ir" (CR_C), "Ir" (0x40)
-	  : "cc");
 }
 
 static inline void platform_do_lowpower(unsigned int cpu)
@@ -194,7 +104,7 @@ static inline void platform_do_lowpower(unsigned int cpu)
 		/*
 		 * here's the WFI
 		 */
-		asm(".word	0xe320f003\n"
+		asm("wfe\n"
 		    :
 		    :
 		    : "memory", "cc");
@@ -223,55 +133,6 @@ static inline void platform_do_lowpower(unsigned int cpu)
 int disable_printk_in_caller=0;
 int platform_cpu_kill(unsigned int cpu)
 {
-#if defined(CONFIG_ARCH_MT5890) ||defined(CONFIG_ARCH_MT5882) ||defined(CONFIG_ARCH_MT5883) || defined(CONFIG_ARCH_MT5891)
-    if (!disable_printk_in_caller)
-        printk("(yjdbg) platform_cpu_kill: %d, ", cpu);
-    cpu = cpu_logical_map(cpu);
-    if (!disable_printk_in_caller)
-        printk("%d\n", cpu);
-
-    if(cpu == 0)
-    {
-        printk(KERN_NOTICE "we should not turn off CPU%u\n", cpu);
-        return 0;
-    }
-
-    if(!(((cpu >= 0) && (cpu <4)) || ((cpu >= 256) && (cpu <258))) )
-    {
-        printk(KERN_NOTICE "we can't handle CPU%u\n", cpu);
-        return 0;
-    }
-    /*
-    // check if CPU is in WFI/WFE
-    *(volatile unsigned int *)0xf0078164 = 0x0;
-    *(volatile unsigned int *)0xf007811c = 0x1b;
-    *(volatile unsigned int *)0xf0078014 = 0x7;
-    regval32 = *(volatile unsigned int *)0xf0078018;
-    while(!(regval32 & (1 << cpu) || regval32 & (1 << (cpu + 4))))
-    {
-		printk(KERN_ERR "CPU%u is not in WFI/WFE mode 0x%x\n", cpu, regval32);
-    }
-    */
-    if (!disable_printk_in_caller)
-        printk(KERN_NOTICE "platform_cpu_kill: CPU%u enter WFI mode\n", cpu);
-
-    if(cpu < 4)
-    {
-        BIM_CpuCA17Down(cpu);
-    }
-    else
-    {
-        if(cpu == 256)
-        {
-            BIM_ClusterDisable(1);
-        }
-        BIM_CpuCA7Down((cpu & 0xff));
-    }
-
-    if (!disable_printk_in_caller)
-        printk(KERN_NOTICE "CPU%u is killed.\n", cpu);
-#endif // CONFIG_ARCH_MT5890
-
     return 1;
 }
 
@@ -383,10 +244,10 @@ void __ref mt53xx_cpu_boot(unsigned int cpu)
     }
 
     printk(KERN_NOTICE "we turn on CPU%u\n", cpu);
-    
+
     printk(KERN_NOTICE "we turn1 on CPU%u\n", cpu);
     platform_smp_prepare_cpus_wakeup();
-	
+
     printk(KERN_NOTICE "we turn2 on CPU%u\n", cpu);
     #endif // CONFIG_ARCH_MT5890
 }
