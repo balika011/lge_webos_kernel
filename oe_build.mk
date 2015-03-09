83e83cffe80e2affc571fca12f623ca494a2e820
p4admin <p4admin@mediatek.com>
2015-03-09 16:39:19 +0800

    [MTWTV-1932][LGE A5LR]debug tool implement . fix build error
    ---- mtk usage begin ---
    MTK_Submitter: hongjun.chu
    MTK_MP_CL: 1942600
    MTK_RLS_CL: 1942643
    MTK_CR_ID: DTV00640653
    ---- mtk usage end ---
    
     [MTWTV-1932][LGE A5LR]debug tool implement
    .
    ---- mtk usage begin ---
    MTK_Submitter: hongjun.chu
    MTK_MP_CL: 1942599
    MTK_RLS_CL: 1942643
    MTK_CR_ID: DTV00640653
    ---- mtk usage end ---
    
     [MTWTV-1932][LGE A5LR]debug tool implement
    .
    add debug version kernel config
    ---- mtk usage begin ---
    MTK_Submitter: hongjun.chu
    MTK_MP_CL: 1942575
    MTK_RLS_CL: 1942643
    MTK_CR_ID: DTV00640653
    ---- mtk usage end ---
    .

--- oe_build.mk
+++ oe_build.mk
@@ -1,6 +1,11 @@
 #Just for verfiyication building in mtk side
+ifeq "$(BUILD_CFG)" "debug"
+DEFAULT_CONFIG := mt5882_A5LR_smp_mod_dbg_defconfig
+else
+DEFAULT_CONFIG := mt5882_A5LR_smp_mod_defconfig
+endif 
 all :
-			make ARCH=arm mt5882_A5LR_smp_mod_defconfig
+			make ARCH=arm $(DEFAULT_CONFIG)
 			make ARCH=arm uImage -j64 CONFIG_CROSS_COMPILE=/mtkoss/gnuarm/arm-lg115x-linux-gnueabi-4.8-2014.08-1-x86_64/bin/arm-lg115x-linux-gnueabi-
 			chmod +x kernel.sh
 			chmod +x ./../boot/uboot/tools/mkepk
